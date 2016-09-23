d613437f480f2a2db60cd1e66edb08dbf7c4e743
tanweijun <twj8892@gmail.com>
2016-09-23 17:02:30 +0800

    Added a preamble when serializing. Packet format is still the same. Still unable to receive data or deserialise correctly.

--- arduino/comms.c
+++ arduino/comms.c
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <Arduino_FreeRTOS.h>
 
 #define MAX_BUF_LEN 23
 #define NUM_DEVICES 4
@@ -36,92 +37,97 @@ void sendData(uint8_t pktType, uint8_t dev1, uint32_t val1,
     .valueof4 = val4
   };
   
-  char buffer[MAX_BUF_LEN];
+  char buffer[MAX_BUF_LEN+2];
 
-  unsigned len = serialize(buffer, &pkt, sizeof(pkt));
-  sendSerialData(buffer, len);
+  serialize(buffer, &pkt, sizeof(pkt));
+  sendSerialData(buffer, sizeof(buffer));
 }
 
 unsigned int serialize(char *buf, void *p, size_t size){
-  uint8_t checksum = 0;
-  memcpy(buf, p, size);
-  for( int i = 0; i < size; i++ ){
+  char checksum = 0;
+  uint8_t startMarker1 = 0xAA; //preamble 0xAAAA
+  uint8_t startMarker2 = 0xAA;
+  buf[0] = startMarker1;
+  buf[1] = startMarker2;
+  memcpy(buf+2, p, size);
+  for( int i = 2; i < size+2; i++ ){
     checksum ^= buf[i];
   }
-  buf[size+1] = checksum;  
-  return size+1;
+  buf[size+3] = checksum;
+  //Serial.print("Serialize Checksum = ");
+  //Serial.println(checksum);
+  return size+3;
 }
 
 void sendSerialData(char *buffer, int len){
   Serial1.write(buffer, len);
-  //Serial1.write('\n');
 }
 
 void receiveData(){
   char buffer[MAX_BUF_LEN];
+  char check[2];
   Packet pkt;
-  
-  /*
-  char endMarker = '\n';
-  char rc;
-  static byte index = 0;
+  char rbyte;
+  int index = 0;
   boolean newData = false; 
-  
-  
-  //Serial1.readBytes(buffer, MAX_BUF_LEN);
-  while (Serial.available() > 0 && newData == false) {
-    rc = Serial.read();
-  
-    if (rc != endMarker) {
-      buffer[index] = rc;
-      index++;
-    }
+
+  while ( Serial.available() > 0 && newData == false ) {
+    rbyte = Serial.read();
+    index = index % 2;
+    check[index] = rbyte;
+    index++;
+    //Serial.print(check[0]);
+    //Serial.println(check[1]);
     
-    if (index >= MAX_BUF_LEN) {
-      index = MAX_BUF_LEN - 1;
-    } else {
-      buffer[index] = '\0'; // terminate the string
-      index = 0;
+    if ( check[0] == (char)0xAA && check[1] == (char)0xAA ){
+      for ( int j = 0; j < 23; j++ ){
+        buffer[j] = Serial.read();    
+      }
       newData = true;
     }
   }
-   
+  
   if (newData == true){
     newData = false;
-    
-    deserialize(&pkt, buffer);
+    unsigned int result = deserialize(&pkt, buffer);
     //process the values here
-    Serial.print("Serial Data: ");
-    Serial.print(pkt.pktType);
-    Serial.print("...");
-    Serial.print(pkt.deviceId1);
-    Serial.print("...");
-    Serial.print(pkt.valueof1);
-    Serial.print("...");
-    Serial.print(pkt.deviceId2);
-    Serial.print("...");
-    Serial.print(pkt.valueof2);
-    Serial.print("...");
-    Serial.print(pkt.deviceId3);
-    Serial.print("...");
-    Serial.print(pkt.valueof3);
-    Serial.print("...");
-    Serial.print(pkt.deviceId4);
-    Serial.print("...");
-    Serial.print(pkt.valueof4);
-    Serial.println("...");
-  }*/
+    if ( result == 1 ){
+      Serial.print("Serial Data: ");
+      Serial.print(pkt.pktType);
+      Serial.print("..");
+      Serial.print(pkt.deviceId1);
+      Serial.print("..");
+      Serial.print(pkt.valueof1);
+      Serial.print("..");
+      Serial.print(pkt.deviceId2);
+      Serial.print("..");
+      Serial.print(pkt.valueof2);
+      Serial.print("..");
+      Serial.print(pkt.deviceId3);
+      Serial.print("..");
+      Serial.print(pkt.valueof3);
+      Serial.print("..");
+      Serial.print(pkt.deviceId4);
+      Serial.print("..");
+      Serial.print(pkt.valueof4);
+      Serial.println("..");
+    } else {
+      Serial.println("Checksum failed?");
+    }
+  }
 }
 
 unsigned int deserialize(void *p, char *buf){
   // numDevices * 5bytes + 1byte pktType + 1byte numDevices
   size_t size = (buf[1] * 5) + 2;
-  uint8_t checksum = 0;
+  char checksum = 0;
 
   for( int i = 0; i < size; i++ ){
     checksum ^= buf[i];
   }
-  
+  //Serial.print("Deserialize Checksum = ");
+  //Serial.println(checksum);
+
   if ( checksum == buf[size+1] ){
     memcpy(p, buf, size);
     return PACKET_OK;
@@ -139,6 +145,6 @@ void setup() {
 void loop() {
   sendData(1, 7, 7, 2, 2, 3, 3, 4, 4); // dummy values for now
   receiveData();
-  sendData(1, 8, 8, 6, 6, 3, 4, 5, 4); // dummy values for now
-  receiveData();
+  //sendData(1, 8, 8, 6, 6, 3, 4, 5, 4); // dummy values for now
+  //receiveData();
 }
