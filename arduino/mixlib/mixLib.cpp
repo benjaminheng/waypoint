4b39068d8b73635dba25675001c54f71cbc0b476
YiHong <yi.hong.lim@gmail.com>
2016-10-18 01:53:47 +0800

    Update mixLib.cpp

--- arduino/mixlib/mixLib.cpp
+++ arduino/mixlib/mixLib.cpp
@@ -95,34 +95,100 @@ int mixLib::getURF()
 	return pulseIn(PIN_URF,HIGH);
 }
 
-int mixLib::getSR04_Front_cm()
+
+int mixLib::getSR08_Front()
 {
-	return (getSR04_Front()/2) / 29.1; 
+  Wire.beginTransmission(0x70);
+  Wire.write(0x01); 
+  Wire.write(0x00); 
+  Wire.endTransmission(); 
+  
+  Wire.beginTransmission(0x70); 
+  Wire.write(0x02); 
+  Wire.write(0x8C);
+  Wire.endTransmission(); 
+
+  int reading = 0;
+  Wire.beginTransmission(0x70); 
+  Wire.write(0);      
+  Wire.write(byte(0x51));      
+  Wire.endTransmission();    
+  delay(70);                   
+  Wire.beginTransmission(0x70);
+  Wire.write(byte(0x02));      
+  Wire.endTransmission();      
+  Wire.requestFrom(0x70,2);
+  if (2 <= Wire.available())   
+  {
+    reading = Wire.read();  
+    reading = reading << 8;    
+    reading |= Wire.read(); 
+    return reading;
+  }
 }
 
-int mixLib::getSR04_Left_cm()
+int mixLib::getSR02_Right()
 {
-	return (getSR04_Left()/2) / 29.1; 
+  Wire.beginTransmission(0x72);
+  Wire.write(0x01); 
+  Wire.write(0x00); 
+  Wire.endTransmission(); 
+  
+  Wire.beginTransmission(0x72);
+  Wire.write(0x02); 
+  Wire.write(0x8C);
+  Wire.endTransmission(); 
+
+  int reading = 0;
+  Wire.beginTransmission(0x72);
+  Wire.write(0);      
+  Wire.write(byte(0x51));      
+  Wire.endTransmission();   
+ 
+  delay(70);                 
+  
+  Wire.beginTransmission(0x72);
+  Wire.write(byte(0x02));      
+  Wire.endTransmission();      
+  Wire.requestFrom(0x72,2);
+  if (2 <= Wire.available())   
+  {
+    reading = Wire.read();  
+    reading = reading << 8;    
+    reading |= Wire.read(); 
+    return reading;
+  }
 }
 
-int mixLib::getSR04_Front()
+int mixLib::getSR02_Left()
 {
-	digitalWrite(PIN_TRIG_SR04_FRONT, LOW);
-	delayMicroseconds(5);
-	digitalWrite(PIN_TRIG_SR04_FRONT, HIGH);
-	delayMicroseconds(10);
-	digitalWrite(PIN_TRIG_SR04_FRONT, LOW);
+  Wire.beginTransmission(0x71);
+  Wire.write(0x01); 
+  Wire.write(0x00); 
+  Wire.endTransmission(); 
+  
+  Wire.beginTransmission(0x71);
+  Wire.write(0x02); 
+  Wire.write(0x8C);
+  Wire.endTransmission(); 
 
-	return pulseIn(PIN_ECHO_SR04_FRONT,HIGH);
-}
-
-int mixLib::getSR04_Left()
-{
-	digitalWrite(PIN_TRIG_SR04_LEFT, LOW);
-	delayMicroseconds(5);
-	digitalWrite(PIN_TRIG_SR04_LEFT, HIGH);
-	delayMicroseconds(10);
-	digitalWrite(PIN_TRIG_SR04_LEFT, LOW);
-
-	return pulseIn(PIN_ECHO_SR04_LEFT,HIGH);
+  int reading = 0;
+  Wire.beginTransmission(0x71);
+  Wire.write(0);      
+  Wire.write(byte(0x51));      
+  Wire.endTransmission();  
+  
+  delay(70);              
+     
+  Wire.beginTransmission(0x71);
+  Wire.write(byte(0x02));      
+  Wire.endTransmission();      
+  Wire.requestFrom(0x71,2);
+  if (2 <= Wire.available())   
+  {
+    reading = Wire.read();  
+    reading = reading << 8;    
+    reading |= Wire.read(); 
+    return reading;
+  }
 }
